6979fe31ecc96df87b89c4e7307ba02d10154cf7
arpit2425 <arpittrivedi2425@gmail.com>
2020-10-30 21:58:32 +0530

    Add author inside book

--- Schema/schema.js
+++ Schema/schema.js
@@ -1,11 +1,11 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 const books = [
-    { title: "Harry Potter", id: "1" },
-    { title: "Invisible Man. Ralph Ellison. ... ", id: "2" },
-    { title: "Beloved", id: "3" },
-    { title: "A Passage to India. E.M. Forster. ... ", id: "4" },
-    { title: "One Hundred Years of Solitude. ... ", id: "5" },
+    { title: "Harry Potter", id: "1" ,authorId:"1"},
+    { title: "Invisible Man. Ralph Ellison. ... ", id: "2" ,authorId:"2"},
+    { title: "Beloved", id: "3",authorId:"3" },
+    { title: "A Passage to India. E.M. Forster. ... ", id: "4" ,authorId:"2"},
+    { title: "One Hundred Years of Solitude. ... ", id: "5",authorId:"1" },
 ];
 const author = [
     { id: '1', name: 'Sam wills', age: 32 },
@@ -24,6 +24,12 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLString },
         title: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve: (parent, args) => {
+                return _.find(author, { id: parent.authorId });
+            }
+        }
     })
 });
 const AuthorType = new GraphQLObjectType({
